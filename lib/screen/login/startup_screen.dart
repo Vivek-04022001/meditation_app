0b477faaf64240628df03d1b55ddc643ceee04d5
vivek <vk285090@gmail.com>
2024-09-28 16:57:33 +0530

    complete login ui

---
+++ lib/screen/login/startup_screen.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:meditation_app/common/color_extension.dart';
+import 'package:meditation_app/common_widgets/round_button.dart';
+import 'package:meditation_app/screen/login/login_screen.dart';
+import 'package:meditation_app/screen/login/signup_screen.dart';
+
+class StartUpScreen extends StatefulWidget {
+  const StartUpScreen({super.key});
+
+  @override
+  State<StartUpScreen> createState() => _StartUpScreenState();
+}
+
+class _StartUpScreenState extends State<StartUpScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.leanBack);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Image.asset(
+            "assets/img/starup_top.png",
+            width: double.maxFinite,
+            fit: BoxFit.fitWidth,
+          ),
+          Spacer(),
+          Text(
+            "We are what we do",
+            style: TextStyle(
+              color: TColor.primaryText,
+              fontSize: 30,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          const SizedBox(height: 15),
+          Text(
+            "Thousands of people are changing their lives through meditation",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: TColor.secondaryText,
+              fontSize: 16,
+            ),
+          ),
+          const Spacer(),
+          RoundButton(
+            title: "SIGN UP",
+            onPressed: () {
+              context.push(const SignUpScreen());
+            },
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "ALREADY HAVE AN ACCOUND?",
+                style: TextStyle(
+                  color: TColor.secondaryText,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  context.push(const LoginScreen());
+                },
+                child: Text(
+                  'LOGIN',
+                  style: TextStyle(
+                    color: TColor.primary,
+                    fontSize: 14,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const Spacer(),
+        ],
+      ),
+    );
+  }
+}
